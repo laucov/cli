8bbea4f67b37378ec781a5e05c24edf4197e94b1
covaleski <covaleski23@gmail.com>
2024-06-28 13:08:05 -0300

    test: refactor `Router` tests

--- tests/RouterTest.php
+++ tests/RouterTest.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace Tests;
 
 use Laucov\Cli\AbstractCommand;
-use Laucov\Cli\OutgoingRequest;
+use Laucov\Cli\AbstractRequest;
 use Laucov\Cli\Router;
 use PHPUnit\Framework\TestCase;
 
@@ -14,60 +14,51 @@ use PHPUnit\Framework\TestCase;
  */
 final class RouterTest extends TestCase
 {
+    /**
+     * Router instance.
+     */
     protected Router $router;
-
-    protected function setUp(): void
-    {
-        $this->router = new Router();
-    }
 
     /**
      * @covers ::addCommand
      */
     public function testCanAddCommand(): void
     {
+        // Mock and add command.
+        $command = $this->createMock(AbstractCommand::class);
         $this->assertSame(
             $this->router,
-            $this->router->addCommand('valid-class', ValidCommand::class),
+            $this->router->addCommand('valid-class', $command::class),
         );
     }
 
     /**
-     * @covers ::route
      * @covers ::getCommand
-     * @uses Laucov\Cli\AbstractRequest::getCommand
+     * @covers ::route
      * @uses Laucov\Cli\AbstractCommand::__construct
-     * @uses Laucov\Cli\OutgoingRequest::setArguments
-     * @uses Laucov\Cli\OutgoingRequest::setCommand
      * @uses Laucov\Cli\Router::addCommand
      */
     public function testCanRoute(): void
     {
-        // Get a request instance.
-        $request = new OutgoingRequest();
-
         // Create example command.
-        $mock = $this
-            ->getMockBuilder(AbstractCommand::class)
-            ->setConstructorArgs(['request' => $request])
-            ->setMockClassName('RouterCommandTest')
-            ->getMockForAbstractClass();
+        $command = $this->createMock(AbstractCommand::class);
 
-        // Route with valid command.
-        $request->setCommand('test-the-router');
-        $request->setArguments([]);
+        // Route with a valid command.
+        $request = $this->createMock(AbstractRequest::class);
+        $request
+            ->method('getCommand')
+            ->willReturn('test-the-router');
         $command = $this->router
-            ->addCommand('test-the-router', $mock::class)
+            ->addCommand('test-the-router', $command::class)
             ->route($request);
-        $this->assertInstanceOf($mock::class, $command);
+        $this->assertInstanceOf($command::class, $command);
 
-        // Route with invalid command.
-        $request->setCommand('inexistent-command');
+        // Route with an unregistered command.
+        $request = $this->createMock(AbstractRequest::class);
+        $request
+            ->method('getCommand')
+            ->willReturn('brake-the-router');
         $this->assertNull($this->router->route($request));
-
-        // Route without command.
-        $this->expectException(\InvalidArgumentException::class);
-        $this->router->route(new OutgoingRequest());
     }
 
     /**
@@ -79,7 +70,8 @@ final class RouterTest extends TestCase
         $message = 'The command class must extend "%s".';
         $message = sprintf($message, AbstractCommand::class);
         $this->expectExceptionMessage($message);
-        $this->router->addCommand('invalid-class', InvalidCommand::class);
+        $invalid_command = $this->createMock(\stdClass::class);
+        $this->router->addCommand('invalid-command', $invalid_command::class);
     }
 
     /**
@@ -92,15 +84,25 @@ final class RouterTest extends TestCase
         $this->expectExceptionMessage($class_name . ' does not exist.');
         $this->router->addCommand('inexistent-class', $class_name);
     }
-}
 
-class ValidCommand extends AbstractCommand
-{
-    public function run(): void
+    /**
+     * @covers ::route
+     */
+    public function testRequestMustHaveACommandName(): void
     {
+        $request = $this->createMock(AbstractRequest::class);
+        $request
+            ->method('getCommand')
+            ->willReturn(null);
+        $this->expectException(\InvalidArgumentException::class);
+        $this->router->route($request);
     }
-}
 
-class InvalidCommand
-{
+    /**
+     * This method is called before each test.
+     */
+    protected function setUp(): void
+    {
+        $this->router = new Router();
+    }
 }

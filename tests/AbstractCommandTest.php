562bcf0c2bdfeec73f3aea5be4ad1a4b5bca942d
covaleski <covaleski23@gmail.com>
2024-07-05 21:04:29 -0300

    test: refactor `AbstractCommandTest`

--- tests/AbstractCommandTest.php
+++ tests/AbstractCommandTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Tests;
 
 use Laucov\Cli\AbstractCommand;
+use Laucov\Cli\AbstractRequest;
 use Laucov\Cli\IncomingRequest;
 use PHPUnit\Framework\TestCase;
 
@@ -16,16 +17,19 @@ final class AbstractCommandTest extends TestCase
     /**
      * @covers ::__construct
      * @covers ::run
-     * @uses Laucov\Cli\AbstractRequest::getArguments
-     * @uses Laucov\Cli\IncomingRequest::__construct
+     * @uses Laucov\Cli\Printer::__construct
      * @uses Laucov\Cli\Printer::colorize
+     * @uses Laucov\Cli\Printer::output
      * @uses Laucov\Cli\Printer::print
      * @uses Laucov\Cli\Printer::printLine
      */
     public function testCanRun(): void
     {
         // Create request.
-        $request = new IncomingRequest(['foo.php', 'greet', 'John']);
+        $request = $this->createMock(AbstractRequest::class);
+        $request
+            ->method('getArguments')
+            ->willReturn(['John']);
 
         // Create command.
         $command = new class ($request) extends AbstractCommand {
